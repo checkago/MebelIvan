2fe68757ceb7a58ea6dc453ee399deed91a96c65
CHECKago <mfalcon@mail.ru>
2024-03-13 18:24:25 +0300

    second commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py makemigrations
-python manage.py migrate --run-syncdb
+#python manage.py makemigrations
+#python manage.py migrate --run-syncdb
 
 gunicorn MebelIvan.wsgi:application --bind 0.0.0.0:8000 --reload  -w 4
